fb5c4460cd29d13bd0783351d5f300b79dd10e2e
Nakul-Shakya <nakul07951@gmail.com>
2025-07-13 16:54:33 +0530

    Configured MongoDB Atlas connection

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,16 @@
 import express from "express";
 import notesRouters from "./routes/notesRoutes.js";
+import { connectDB } from "./config/db.js";
+import dotenv from "dotenv";
+
 const app = express();
+dotenv.config();
+connectDB();
 
 app.use("/api/notes", notesRouters);
-app.use("/api/product", productRoutes);
+
+
+
 
 app.listen(3000, () => {
   console.log("Sercer started on PORT: 3000");
