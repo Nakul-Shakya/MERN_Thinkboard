f285eb7873164a418e37d71ad659f9912c7ad340
Nakul-Shakya <nakul07951@gmail.com>
2025-07-15 23:25:05 +0530

    Middleware & Rate Limiting

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,21 +2,21 @@ import express from "express";
 import notesRouters from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
+import rateLimiter from "./middleware/rateLimiter.js";
 
 const app = express();
 const PORT = process.env.PORT || 5001;
 dotenv.config();
-connectDB();
+
 
 // Middleware
-app.use(express.json())
-
+app.use(express.json());
+app.use(rateLimiter);
 
 app.use("/api/notes", notesRouters);
 
-
-
-
-app.listen(PORT, () => {
-  console.log(`Sercer started on PORT: ${PORT}`);
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Sercer started on PORT: ${PORT}`);
+  });
 });
