c68bf58bc3903b98d449b63a32967cd603e28e52
Nakul-Shakya <nakul07951@gmail.com>
2025-07-20 00:56:47 +0530

    HomePage err solve

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Routes, Route } from "react-router";
-import { toast } from "react-hot-toast";
+
 import HomePage from "./pages/HomePage";
 import CreatePage from "./pages/CreatePage";
 import NoteDetailPage from "./pages/NoteDetailPage";
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -44,7 +44,7 @@ const HomePage = () => {
           <div className="text-center text-primary py-10">Loading notes...</div>
         )}
         {notes.length > 0 && !isRateLimited && (
-          <div className="grid` grid-cols-1 md:gridco2 lg:grid-cols-3 gap-6">
+          <div className="grid grid-cols-1 md:gridco2 lg:grid-cols-3 gap-6">
             {notes.map((note) => (
               <NoteCard key={note._id} note={note} />
             ))}
