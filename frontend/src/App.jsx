a59e95064216598cbcf5687d97ccc996ee2e3871
Nakul-Shakya <nakul07951@gmail.com>
2025-07-20 01:19:16 +0530

    feat: complete HomePage with card and time functionality, no errors

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,7 +7,11 @@ import NoteDetailPage from "./pages/NoteDetailPage";
 
 const App = () => {
   return (
-    <div data-theme="forest">
+    <div data-theme="forest" className="relative h-full w-full">
+      {/* Background Gradient */}
+      <div className="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_60%,#00FF9D40_100%)]" />
+
+      {/* Route Pages */}
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/create" element={<CreatePage />} />
