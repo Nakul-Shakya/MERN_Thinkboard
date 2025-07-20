2bcfbf59d32036616ea53f1d172b9877bba3b2e8
Nakul-Shakya <nakul07951@gmail.com>
2025-07-20 15:53:56 +0530

    CreatePage UI is 90% DONE

--- frontend/src/pages/CreatePage.jsx
+++ frontend/src/pages/CreatePage.jsx
@@ -1,9 +1,70 @@
-import React from 'react'
+import React, { useState } from "react";
+import { ArrowLeftIcon } from "lucide-react";
+import { Link } from "react-router";
 
 const CreatePage = () => {
-  return (
-    <div>CreatePage</div>
-  )
-}
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+  const [loading, setLoading] = useState(false);
 
-export default CreatePage
+  const handleSubmit = () => {};
+
+  return (
+    <div className="min-h-screen bg-base-200">
+      <div className="container mx-auto px-4 py-8">
+        <div className="max-w-2xl mx-auto">
+          <Link to={"/"} className="btn btn-ghost mb-6">
+            <ArrowLeftIcon className="size-5" />
+            Back to Notes
+          </Link>
+
+          <div className="card bg-base-100">
+            <div className="card-body">
+              <h2 className="card-title text-2xl mb-4">Create New Note</h2>
+              <form onSubmit={handleSubmit}>
+                <div className="form-control mb-4">
+                  <label className="label">
+                    <span className="label-text">Title</span>
+                  </label>
+                  <br />
+                  <input
+                    type="text"
+                    placeholder="Note Title"
+                    className="input input-bordered mt-4 ml-4"
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}
+                  />
+                </div>
+
+                <div className="form-control mb-4">
+                  <label htmlFor="" className="label">
+                    <span className="label-text">Content</span>
+                  </label>
+                  <br />
+                  <textarea
+                    placeholder="Write your note hear..."
+                    className="textarea textarea-bordered h-32 mt-4 ml-4"
+                    value={content}
+                    onChange={(e) => setContent(e.target.value)}
+                  />
+                </div>
+
+                <div className="card-actions justify-end">
+                  <button
+                    type="submit"
+                    className="btn btn-primary"
+                    disabled={loading}
+                  >
+                    {loading ? "Creating..." : "Create Note"}
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CreatePage;
