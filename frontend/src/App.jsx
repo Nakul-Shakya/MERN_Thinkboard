f1753b7c8e7f9743954a4b8c55852db0aa4686fe
Nakul-Shakya <nakul07951@gmail.com>
2025-07-22 22:56:30 +0530

    debug: added error logger to trace deployment crash

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -12,7 +12,7 @@ const App = () => {
       {/* Background Gradient */}
       <div className="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_60%,#00FF9D40_100%)]" />
 
-      {/* Route Pages */}
+      {/* Route Pages */}Z
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/create" element={<CreatePage />} />
