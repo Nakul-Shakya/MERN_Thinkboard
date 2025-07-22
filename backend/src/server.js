ae95177719da881096b4fac6682e39bc0462b19e
Nakul-Shakya <nakul07951@gmail.com>
2025-07-22 22:29:27 +0530

    debug: added error logger to trace deployment crash

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,13 @@
 import express from "express";
-import notesRouters from "./routes/notesRoutes.js";
-import { connectDB } from "./config/db.js";
-import dotenv from "dotenv";
-import rateLimiter from "./middleware/rateLimiter.js";
 import cors from "cors";
+import dotenv from "dotenv";
 import path from "path";
+
+import notesRoutes from "./routes/notesRoutes.js";
+import { connectDB } from "./config/db.js";
+
+import rateLimiter from "./middleware/rateLimiter.js";
+
 
 dotenv.config();
 
@@ -12,29 +15,29 @@ const app = express();
 const PORT = process.env.PORT || 5001;
 const __dirname = path.resolve();
 
-// Middleware
-app.use(express.json());
-app.use(rateLimiter);
+// middleware
 if (process.env.NODE_ENV !== "production") {
   app.use(
     cors({
-      origin: "http://localhost:5173",
-      credentials: true,
+      origin: "http://localhost:5173"
     })
   );
 }
+app.use(express.json()); // this middleware will parse JSON bodies: req.body
+app.use(rateLimiter);
 
-app.use("/api/notes", notesRouters);
-app.use(express.static(path.join(__dirname, "../frontend/dist")));
+app.use("/api/notes", notesRoutes);
 
 if (process.env.NODE_ENV === "production") {
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend/", "dist", "index.html"));
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("/*any", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
 }
 
 connectDB().then(() => {
   app.listen(PORT, () => {
-    console.log(`Sercer started on PORT: ${PORT}`);
+    console.log("Server started on PORT:", PORT);
   });
 });
