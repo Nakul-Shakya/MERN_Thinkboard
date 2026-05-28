a7e82942f8c27fe3b6a121614829021ea8ffd090
Nakul_Shakya <opsd6722@gmail.com>
2026-05-28 18:15:00 +0530

    Update server.js rateLimiter  off

--- backend/src/server.js
+++ backend/src/server.js
@@ -24,7 +24,7 @@ if (process.env.NODE_ENV !== "production") {
   );
 }
 app.use(express.json()); // this middleware will parse JSON bodies: req.body
-app.use(rateLimiter);
+// app.use(rateLimiter); off
 
 app.use("/api/notes", notesRoutes);
 
